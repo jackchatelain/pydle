c10a4d97735115ca5e335145aa4b6afa9506fc9e
Jack C <jackchatelain@gmail.com>
2022-03-07 19:45:54 -0700

    Added color reveal animation

--- pydle.py
+++ pydle.py
@@ -21,33 +21,44 @@ while playing:
         print("")
 
     def coolerPrint(out):
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n------------------------------------------------\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        insertSpace()
         fullOutput.append(out)
         for item in fullOutput:
             print(item)
 
+    def insertSpace():
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n------------------------------------------------\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
     def update(out):
         #fullOutput[-1:] = out
         for item in fullOutput:
-            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n------------------------------------------------\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+            insertSpace()
             print(item)
             sleep(0.2)
 
     for guessNum in range(6):
         guess = input("Guess #" + str(guessNum + 1) + ": ").strip()
         output = ""
+        latest = ""
         if len(guess) == 5 and guess in words:
             for i in range(5):
                 if guess[i] == answer[i]:
                     output += colored(guess[i], "green")
+                    latest = colored(guess[i], "green")
                     emojis += "🟩"
                 elif guess[i] in answer:
                     output += colored(guess[i], "yellow")
+                    latest = colored(guess[i], "yellow")
                     emojis += "🟨"
                 else:
                     output += guess[i]
+                    latest = colored(guess[i], "white")
                     emojis += "⬛️"
+                insertSpace()
+                print(output)
+                sleep(0.3)
                 # update(output)
+            insertSpace()
             coolerPrint(output)
             emojis += "\n"
             if str(guess).strip() == str(answer).strip():
@@ -66,7 +77,7 @@ while playing:
         print("The word was " + answer + "!")
         sleep(0.5)
     print("Your result: \n")
-    print("pydle (Word #" + str(wordNum) + ")")
+    print("pydle " + str(wordNum) + " " + str(guessNum + 1) + "/6")
     print(emojis)
     print("\n")
     ask = input("Do you want to play again? ")
